7f629967baec130400638837a88b66b1373ce798
scott clark <scott@design-monkey.co.uk>
2021-12-15 11:32:41 +0000

    button added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header/Header';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
+
+  //set the component variables
+  const [number, setNumber] = useState(1)
+
+  const incrementNumber = (e) => {
+    e.preventDefault();
+    //set State
+    setNumber(currentNumber =>
+      currentNumber + 1
+    )
+  }
+
   return (
     <div className="App">
+
+      <Header text={number} />
+
+      <button onClick={e => incrementNumber(e)}> Standard Add </button>
+      <Button
+        variant="outlined"
+        color="error"
+        onClick={e => incrementNumber(e)
+        } > 
+        Material ui Add
+      </Button>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
